f2848c47eecc84ac22c0fbd50c6fd683d7c63f86
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2022-02-05 15:59:21 +0530

    third commit from the usingReducerHook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { useState, useReducer } from 'react';
-// import UserBar from './src/user/UserBar';
-// import CreatePost from './src/post/CreatePost';
-// import PostList from './src/post/PostList';
-// import appReducer from './src/Reducer';
-import Login from './hooksCourse/loginForm/Login';
+import UserBar from './src/user/UserBar';
+import CreatePost from './src/post/CreatePost';
+import PostList from './src/post/PostList';
+import appReducer from './src/Reducer';
 
 export default function App (){
-  // const defaultposts = [
-  //   { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
-  //   { title: 'Using React Fragments', content: 'Keeping the DOMtree clean!', author: 'Daniel Bugl' }
-  //  ]
-  //  const [state, dispatch] = useReducer(appReducer, {user : '', posts : defaultposts});
-  //  const [user, posts] = state;
+  const defaultposts = [
+    { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
+    { title: 'Using React Fragments', content: 'Keeping the DOMtree clean!', author: 'Daniel Bugl' }
+   ]
+   //below is the line used for using the reducer
+   const [state, dispatch] = useReducer(appReducer, {user : '', posts : defaultposts});
+   const [user, posts] = state;
    return(
     <>
     <h1>This is the app page</h1>
     <div style = {{padding : 8}}>
-      {/* <UserBar user = {user} dispatch = {dispatch}/>
+      <UserBar user = {user} dispatch = {dispatch}/>
       <br />
       {user && <CreatePost user = {user} posts = {posts} dispatch = {dispatch} />}
-      <PostList posts = {posts} /> */}
-      <Login />
+      <PostList posts = {posts} />
     </div>
     </>
   );
