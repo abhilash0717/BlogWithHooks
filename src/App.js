53f9b7a8faf11ebf64d37e5950664011a864bafe
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2022-02-06 16:51:50 +0530

    new commit from the use effct hook branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { useState, useReducer } from 'react';
-import UserBar from './src/user/UserBar';
-import CreatePost from './src/post/CreatePost';
-import PostList from './src/post/PostList';
-import appReducer from './src/Reducer';
+import React, { useReducer, useEffect } from 'react'
 
-export default function App (){
-  const defaultposts = [
+import PostList from './post/PostList'
+import CreatePost from './post/CreatePost'
+import UserBar from './user/UserBar'
+import appReducer from './reducers'
+
+const defaultPosts = [
     { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
-    { title: 'Using React Fragments', content: 'Keeping the DOMtree clean!', author: 'Daniel Bugl' }
-   ]
-   //below is the line used for using the reducer
-   const [state, dispatch] = useReducer(appReducer, {user : '', posts : defaultposts});
-   const [user, posts] = state;
-   return(
-    <>
-    <h1>This is the app page</h1>
-    <div style = {{padding : 8}}>
-      <UserBar user = {user} dispatch = {dispatch}/>
-      <br />
-      {user && <CreatePost user = {user} posts = {posts} dispatch = {dispatch} />}
-      <PostList posts = {posts} />
-    </div>
-    </>
-  );
+    { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
+]
+
+export default function App () {
+    const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts })
+    const { user, posts } = state
+    
+    useEffect(()=>{ 
+        if(user){
+            document.title = `${user} -- React Hooks Blog`;
+        }else{
+            document.title = "react hooks blog";
+        }
+    }, [user])
+    return (
+        <div style={{ padding: 8 }}>
+            <UserBar user={user} dispatch={dispatch} />
+            <br />
+            {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
+            <br />
+            <hr />
+            <PostList posts={posts} />
+        </div>
+    )
 }
